5fb532ca580e5b6f09ffbade752239e1826c2073
Andrey.S <161717784+Nevmen9Iemm@users.noreply.github.com>
2024-12-07 01:12:22 +0200

    edit pytest CRUD

--- tests/test_CRUD.py
+++ tests/test_CRUD.py
@@ -30,8 +30,8 @@ def test_crud_operations(test_db):
     session.add(category)
     session.commit()
 
-    product1 = Product(name="Apple", price=1.50, image="apple.jpg", category_id=2)
-    product2 = Product(name="Banana", price=2.00, image="banana.jpg", category_id=3)
+    product1 = Product(name="Apple", price=1.50, image="apple.jpg", category_id=)
+    product2 = Product(name="Banana", price=2.00, image="banana.jpg", category_id=category.id)
     session.add_all([product1, product2])
     session.commit()
     assert session.query(Product).count() == 2
