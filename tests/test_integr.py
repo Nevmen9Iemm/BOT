a566436ef8d4646042da7f72eadcdf41480ff7f2
Andrey.S <161717784+Nevmen9Iemm@users.noreply.github.com>
2024-12-07 00:29:48 +0200

    edit test integr

--- tests/test_integr.py
+++ tests/test_integr.py
@@ -3,8 +3,16 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_dispatcher_includes_routers():
-    from app import dp, user_private_router, user_group_router, admin_router
+    # from app import dp, user_private_router, user_group_router, admin_router
+    #
+    # assert user_private_router in dp.routers
+    # assert user_group_router in dp.routers
+    # assert admin_router in dp.routers
 
-    assert user_private_router in dp.routers
-    assert user_group_router in dp.routers
-    assert admin_router in dp.routers
+    from app import dp
+    from handlers import admin_private, user_group, user_private, menu_processing
+
+    assert admin_private.router in dp.routers
+    assert user_group.router in dp.routers
+    assert user_private.router in dp.routers
+    assert menu_processing.router in dp.routers
